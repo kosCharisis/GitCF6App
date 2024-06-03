c525d110d71cef9d828101f5ca2e4af1a30aea94
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-03 20:59:39 +0300

    Point Inheritance

---
+++ src/gr/aueb/cf/ch15/Point2D.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.ch15;
+
+public class Point2D extends Point {
+    private double y;
+
+    public Point2D() {
+        // super();
+        // y = 0;
+    }
+
+    public Point2D(double x, double y) {
+        super(x);
+        //super();
+        //setX(x);
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String convertToString() {
+        return "(" + getX() + ", " + y + ")";
+    }
+}
