0d2186b22b720d4706bace35983647d142513d81
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-04 18:57:10 +0300

    Point Inheritance

---
+++ src/gr/aueb/cf/ch15/person/Main.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch15.person;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee alice = new Employee(1, "Alice", "W.", 10000);
+
+        alice.work();
+        alice.speak();
+
+        System.out.println("Firstname: " + alice.getFirstname() + " Lastname: "
+            + alice.getLastname() + " Salary: " + alice.getSalary());
+    }
+}
