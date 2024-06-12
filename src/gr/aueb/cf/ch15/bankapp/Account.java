e9b9052787e13ec4630c6e970089ac0460486ca3
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-12 19:29:40 +0300

    Interfaces

--- src/gr/aueb/cf/ch15/bankapp/Account.java
+++ src/gr/aueb/cf/ch15/bankapp/Account.java
@@ -18,14 +18,13 @@ public class Account extends IdentifiableEntity {
     public Account(String iban, User holder, double balance) {
         //super();
         this.iban = iban;
-        this.holder = holder;
+        this.holder = new User(holder);
         this.balance = balance;
     }
 
     public String getIban() {
         return iban;
     }
-
     public void setIban(String iban) {
         this.iban = iban;
     }
@@ -33,7 +32,6 @@ public class Account extends IdentifiableEntity {
     public User getHolder() {
         return new User(holder);
     }
-
     public void setHolder(User holder) {
         this.holder = new User(holder);
     }
@@ -41,7 +39,6 @@ public class Account extends IdentifiableEntity {
     public double getBalance() {
         return balance;
     }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
