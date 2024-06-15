7ddcbc4576fcc5b58e532403fa4ce1ec3e15f9c9
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-15 21:14:41 +0300

    14 exer

--- src/gr/aueb/cf/ch14/StringUtils.java
+++ src/gr/aueb/cf/ch14/StringUtils.java
@@ -3,5 +3,29 @@ package gr.aueb.cf.ch14;
 public class StringUtils {
     private StringUtils() {}
 
+    public static String reverse(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        return rev;
+    }
+
+    public static String uppercase(String s) {
+        String upper = s.toUpperCase();
+        return upper;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int n = s.length();
+        for (int i = 0; i < (n/2); ++i) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAlsoPalindrome(String s) {
+        boolean equal = reverse(s).equals(s);
+        return equal;
+    }
 
 }
