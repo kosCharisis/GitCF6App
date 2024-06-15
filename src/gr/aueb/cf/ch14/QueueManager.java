b8ffaf3a5cd73207caed26dea71743e5447369b8
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-15 22:35:54 +0300

    14 exer

---
+++ src/gr/aueb/cf/ch14/QueueManager.java
@@ -0,0 +1,48 @@
+package gr.aueb.cf.ch14;
+
+public class QueueManager {
+    private static final QueueManager INSTANCE = new QueueManager();
+
+    private QueueManager() {}
+
+    public static QueueManager getInstance() {
+        return INSTANCE;
+    }
+
+    static int[] queue = new int[100];
+    static int pivot = -1;
+    public static void enQueue(int num) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return;
+        }
+        queue[++pivot] = num;
+    }
+
+    public static int deQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return 0;
+        }
+
+        int valueToReturn = queue[0];
+        pivot--;
+        shiftQueueLeft();
+        return valueToReturn;
+    }
+
+    public static void shiftQueueLeft() {
+        for (int i = 0; i <= pivot; i++) {
+            queue[i] = queue[i+1];
+        }
+    }
+
+    public static boolean isEmpty() {
+        return pivot == -1;
+    }
+
+    public static boolean isFull() {
+        return pivot == queue.length - 1;
+    }
+
+}
