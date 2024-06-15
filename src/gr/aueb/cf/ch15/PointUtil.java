08377708039fbb03391dd0121f81903480fc445f
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-15 19:26:18 +0300

    Points exer

--- src/gr/aueb/cf/ch15/PointUtil.java
+++ src/gr/aueb/cf/ch15/PointUtil.java
@@ -1,4 +1,17 @@
 package gr.aueb.cf.ch15;
 
 public class PointUtil {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(6.5);
+        Point p2 = new Point2D(3, 4);
+        Point p3 = new Point3D(3, 4, 5);
+
+      System.out.println("Distance from origin: " + distanceFromOrigin(p1));
+      System.out.println("Distance from origin: " + distanceFromOrigin(p2));
+      System.out.println("Distance from origin: " + distanceFromOrigin(p3));
+    }
+    public static double distanceFromOrigin(Point point) {
+        return point.getDistanceFromOrigin();
+    }
 }
