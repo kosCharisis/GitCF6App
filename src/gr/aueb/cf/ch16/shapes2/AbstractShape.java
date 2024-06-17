b20d33dbf429aaf34cb1ca4ec74506da4d60e702
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-17 20:02:17 +0300

    shapes

---
+++ src/gr/aueb/cf/ch16/shapes2/AbstractShape.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.ch16.shapes2;
+
+public class AbstractShape implements IShape{
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
---
+++ src/gr/aueb/cf/ch16/shapes2/Circle.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch16.shapes2;
+
+public class Circle extends AbstractShape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
