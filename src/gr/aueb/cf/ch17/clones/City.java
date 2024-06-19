8bb3c7aa4a6c124ece7645fa819d1ee3e8e429a9
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-19 21:59:36 +0300

    intro equals and hash

---
+++ src/gr/aueb/cf/ch17/clones/City.java
@@ -0,0 +1,57 @@
+package gr.aueb.cf.ch17.clones;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class City implements Cloneable, Serializable {
+    private static long serialVersionUID = -12345789L;
+    private String description;
+    private transient long hashCode;
+
+    public City() {
+
+    }
+
+    public City(String description) {
+        this.description = description;
+    }
+
+    //Copy Constructor
+
+    public City(City city) {
+        this.description = city.getDescription();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "City: " + description;
+    }
+
+    @Override
+    protected City clone() throws CloneNotSupportedException {
+        return (City) super.clone();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        City city = (City) other;
+
+        return this.getDescription().equals(city.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDescription());
+    }
+}
---
+++ src/gr/aueb/cf/ch17/clones/Main.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.ch17.clones;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+//        City athens = new City("Athens");
+//
+//        City clonedAthens = athens.clone();
+//        clonedAthens.setDescription("Paris");
+//
+//        System.out.println(athens);
+//        System.out.println(clonedAthens);
+
+        Trainee alice = new Trainee("Alice", new City("Athens"));
+//        Trainee clonedAlice = alice.clone();
+//
+//        clonedAlice.getCity().setDescription("London");
+//
+//        System.out.println(alice);
+//        System.out.println(clonedAlice);
+
+        Trainee clonedAlice = new Trainee(alice);
+        clonedAlice.getCity().setDescription("London");
+
+        System.out.println(alice);
+        System.out.println(clonedAlice);
+
+
+
+    }
+
+
+
+
+
+
+
+}
