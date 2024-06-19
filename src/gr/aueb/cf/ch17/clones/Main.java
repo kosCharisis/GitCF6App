cf8fbf7e9ec4e404445b29b9456281dcf0581b99
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-19 22:10:15 +0300

    intro equals and hash

--- src/gr/aueb/cf/ch17/clones/Main.java
+++ src/gr/aueb/cf/ch17/clones/Main.java
@@ -20,20 +20,36 @@ public class Main {
 //        System.out.println(alice);
 //        System.out.println(clonedAlice);
 
-        Trainee clonedAlice = new Trainee(alice);
-        clonedAlice.getCity().setDescription("London");
+//        Trainee clonedAlice = new Trainee(alice);
+//        clonedAlice.getCity().setDescription("London");
+//
+//        System.out.println(alice);
+//        System.out.println(clonedAlice);
 
-        System.out.println(alice);
-        System.out.println(clonedAlice);
 
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("C:/tmp/trainee.ser"))) {
+            oos.writeObject(alice);
+        } catch (NotSerializableException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:/tmp/trainee.ser"))) {
+            Trainee aliceTrainee = (Trainee) ois.readObject();
+            System.out.println(aliceTrainee);
+        } catch (NotSerializableException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
-
-
-
 
 
 }
