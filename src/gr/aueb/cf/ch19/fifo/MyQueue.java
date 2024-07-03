10f7d0ffc9c900935c74beb87f2a47208a932ddb
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-07-04 01:19:44 +0300

    sets

---
+++ src/gr/aueb/cf/ch19/fifo/MyQueue.java
@@ -0,0 +1,41 @@
+package gr.aueb.cf.ch19.fifo;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MyQueue<T> {
+    private final Deque<T> myQueue = new LinkedList<>();
+
+    public MyQueue() {
+    }
+
+    public void enQueue(T t) {
+        myQueue.addLast(t);
+    }
+
+    public T deQueue() {
+        return myQueue.poll();
+    }
+
+    public int getSize() {
+        return myQueue.size();
+    }
+
+    public boolean isEmpty() {
+        return myQueue.isEmpty();
+    }
+// για το service
+
+//     try {
+//        if (myQueue.isEmpty()) {
+//            throw new QueueIsEmptyException("empty queue");
+//        }
+//        //log
+//        return myQueue.poll(); // extracts from the start
+//    } catch (QueueIsEmptyException e) {
+//        // log
+//        throw e;
+//    }
+
+
+}
---
+++ src/gr/aueb/cf/ch19/sets/TreeSetDemo.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.ch19.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+    public static void main(String[] args) {
+        // add, remove, contains time complexity -> O(n)
+        Set<String> bag = new TreeSet<>();
+        //bag.add("Honey");
+
+        if (bag.contains("Milk")) {
+            System.out.println("milk is already added.");
+        } else {
+            if (bag.add("Milk")) System.out.println("Success insert");
+        }
+
+          bag.add("Honey");
+//
+//        if (bag.remove("Milk")) {
+//            System.out.println("Successfully removed");
+//        } else {
+//            System.out.println("Error in removal.");
+//        }
+
+        bag.forEach(System.out::println);
+    }
+}
